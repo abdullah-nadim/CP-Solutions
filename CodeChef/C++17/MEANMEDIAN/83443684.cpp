e093683b07b735b2e3c9d80057e0a0a2d9b4f0eb
MD NURE ALAM NADIM <nadim.soft.111@gmail.com>
2023-01-13 00:13:03 +0600

    Committing files

---
+++ CodeChef/C++17/MEANMEDIAN/83443684.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,x,y;
+	    cin>>x>>y;
+	        a=-501;
+	        b=y;
+	        c=3*x-b-a;
+     cout<<a<<" "<<b<<" "<<c<<endl;
+	   
+	}
+	return 0;
+}

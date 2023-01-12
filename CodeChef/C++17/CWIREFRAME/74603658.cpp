a51fdaa072a2e4add2d42a35b9199507fca1fb0e
MD NURE ALAM NADIM <nadim.soft.111@gmail.com>
2023-01-13 00:15:23 +0600

    Committing files

---
+++ CodeChef/C++17/CWIREFRAME/74603658.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m,x,a,s;
+	    cin>>n>>m>>x;
+	    a=2*n+2*m;
+	    s=x*a;
+	    cout<<s<<"\n";
+	}
+	return 0;
+}

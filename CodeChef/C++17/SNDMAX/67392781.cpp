a24c28f8516447864ac58c22cb7778966ce25ca5
MD NURE ALAM NADIM <nadim.soft.111@gmail.com>
2023-01-13 00:15:54 +0600

    Committing files

---
+++ CodeChef/C++17/SNDMAX/67392781.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,a,b,c;
+	cin>>n;
+	while(n--)
+	{
+	    int arr[3];
+	    for (int i =0;i<3;i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    sort(arr, arr + 3);
+	    cout<<arr[1]<<endl;
+	}
+	return 0;
+}

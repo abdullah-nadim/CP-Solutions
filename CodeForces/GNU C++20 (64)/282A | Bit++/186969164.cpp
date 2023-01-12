240b4ce505f6a7d488326ccd4db05d742f20a3fa
MD NURE ALAM NADIM <nadim.soft.111@gmail.com>
2023-01-13 00:17:37 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/282A | Bit++/186969164.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int t,x=0;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        if(s=="X++")
+            x++;
+        else if(s=="X--")
+            x--;
+        else if(s=="--X")
+            --x;
+        else if(s=="++X")
+            ++x;
+    }
+    cout<<x;
+}

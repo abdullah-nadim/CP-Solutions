25f2b2c1ceae20e3ece82b370aa1e2112e41b8dd
MD NURE ALAM NADIM <nadim.soft.111@gmail.com>
2023-01-13 00:15:18 +0600

    Committing files

---
+++ CodeChef/C++17/CCHOCOLATES/74603764.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int a,x,y,z,c;
+	    cin>>x>>y>>z;
+	    a=x*5+10*y;
+	    c=a/z;
+	    cout<<c<<"\n";
+	}
+	return 0;
+}
